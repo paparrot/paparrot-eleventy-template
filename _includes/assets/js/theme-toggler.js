7fe48aa9f34c757550887a57ca34c95302030eb1
Klim <59335849+paparrot@users.noreply.github.com>
2021-08-18 15:51:36 +0300

    fix toggler in dark theme

--- _includes/assets/js/theme-toggler.js
+++ _includes/assets/js/theme-toggler.js
@@ -1,12 +1,14 @@
 const toggler = document.getElementById('theme-toggler');
 const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const isLocalEmpty = localStorage.getItem('theme') === null;
 const isLocalDark = localStorage.getItem('theme') === 'dark';
 
-let isDark = isLocalDark || isSystemDark;
+let isDark = isLocalEmpty ? isSystemDark : isLocalDark;
 if (isDark) document.documentElement.classList.add('dark');
 
 toggler.addEventListener('click', () => {
-  document.documentElement.classList.toggle('dark');
   isDark = !isDark;
-  isDark ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
+  isDark ? localStorage.setItem("theme","dark") : localStorage.setItem("theme","light");
 });
+
+const toggleTheme
