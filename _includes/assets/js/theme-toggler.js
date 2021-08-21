c87362118aa2c3bd56d41503b9c969bd4e3426e3
Klim <59335849+paparrot@users.noreply.github.com>
2021-08-21 11:50:48 +0300

    fix theme for cusdis

--- _includes/assets/js/theme-toggler.js
+++ _includes/assets/js/theme-toggler.js
@@ -1,15 +1,22 @@
 const toggler = document.getElementById('theme-toggler');
 const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const isLocalEmpty = localStorage.getItem('theme') === null;
 const isLocalDark = localStorage.getItem('theme') === 'dark';
 
-let isDark = isLocalEmpty ? isSystemDark : isLocalDark;
-if (isDark) document.documentElement.classList.add('dark');
+const isDark = isLocalDark || isSystemDark;
+let theme = isDark ? 'dark' : 'light';
+localStorage.setItem('theme', theme);
+if (theme === 'dark') document.documentElement.classList.add('dark');
+
+if (document.getElementById('cusdis_thread')) {
+  document.getElementById('cusdis_thread').dataset.theme = theme;
+}
 
 const toggleTheme = () => {
-  isDark = !isDark;
-  isDark ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
   document.documentElement.classList.toggle('dark');
+  theme = theme == 'dark' ? 'light' : 'dark';
+  localStorage.setItem('theme', theme);
+
+  window?.CUSDIS?.setTheme(theme);
 };
 
 toggler.addEventListener('click', toggleTheme);
